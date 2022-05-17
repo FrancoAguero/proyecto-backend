b8e62d7d04d18ba5fdede91416f6016dda561479
FrancoAguero <franko465aguero@gmail.com>
2022-05-17 20:44:25 -0300

    port

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { ServerApiVersion } from 'mongodb';
 import 'dotenv/config';
 
 export default {
-    PORT: process.env.PORT || 3001,
+    PORT: process.env.PORT || 8080,
     mongoRemote: {
         client: { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 },
         cnxStr: `mongodb+srv://francoPlomer:${process.env.pass}@cluster0.fihir.mongodb.net/ecommerce?retryWrites=true&w=majority`
